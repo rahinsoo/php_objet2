aaaa739e578f17fa9bdcaa885d48a197c966b263
rahinsoo <rahinsoo@etik.com>
2025-11-06 12:25:04 +0100

    TP02-PDO | sécurité dans le formulaire

--- TP02-PDO/formulaire.php
+++ TP02-PDO/formulaire.php
@@ -17,26 +17,85 @@
     </div>
 </header>
 <main>
-    <form action="traitement.php"  method="post">
+    <?php
+    // Initialisation des variables
+    $nom = $email = $sujet = $message = null;
+
+    // Vérification de la soumission du formulaire et de la présence des champs
+    if (isset($_POST['nom'], $_POST['mail'], $_POST['sujet'], $_POST['message'])) {
+
+        // Récupération des données POST
+        $nom = $_POST['nom'];
+        $email = $_POST['mail'];
+        $sujet = $_POST['sujet'];
+        $message = $_POST['message'];
+    }
+    ?>
+    <form action="formulaire.php" method="POST">
         <div class="form-group">
             <label for="nom" class="form-label text-light">Ton nom : </label>
-            <input type="text" class="form-control" id="nom" name="nom" aria-describedby="nomHelp">
+            <input type="text" class="form-control" value="<?php if (isset($_POST['nom'])) {
+                echo htmlentities($_POST['nom']);
+            } ?>" id="nom" name="nom" aria-describedby="nomHelp">
+            <?php
+            if (isset($_POST['nom']) && empty($_POST['nom'])) {
+                echo "<p style='color:white;'>le mail est vide </p>";
+            }
+            ?>
         </div>
         <div class="mb-3">
             <label for="mail" class="form-label text-light">Email address</label>
-            <input type="email" class="form-control" id="mail" name="mail" aria-describedby="emailHelp">
+            <input type="email" class="form-control" value="<?php if (isset($_POST['mail'])) {
+                echo htmlentities($_POST['mail']);
+            } ?>" id="mail" name="mail" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text text-light">We'll never share your email with anyone else.</div>
+            <?php
+            if (isset($_POST['mail']) && empty($_POST['mail'])) {
+                echo "<p style='color:white;'>le nom est vide </p>";
+            }
+            ?>
         </div>
         <div class="mb-3">
             <label for="sujet" class="form-label text-light">Ton sujet : </label>
-            <input type="text" class="form-control" id="sujet" name="sujet" aria-describedby="nomHelp">
+            <input type="text" class="form-control" value="<?php if (isset($_POST['sujet'])) {
+                echo htmlentities($_POST['sujet']);
+            } ?>" id="sujet" name="sujet" aria-describedby="nomHelp">
+            <?php
+            if (isset($_POST['sujet']) && empty($_POST['sujet'])) {
+                echo "<p style='color:white;'>le sujet est vide </p>";
+            }
+            ?>
         </div>
         <div class="form-group">
             <label for="exampleFormControlTextarea1" class="form-label text-light">Ton texte : </label>
-            <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <textarea class="form-control" name="message" value="<?php if (isset($_POST['message'])) {
+                echo htmlentities($_POST['message']);
+            } ?>" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <?php
+            if (isset($_POST['message']) && empty($_POST['message'])) {
+                echo "<p style='color:white;'>le message est vide </p>";
+            }
+            ?>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+    <?php
+    // Vérification de la soumission du formulaire et de la présence des champs
+    if (isset($nom, $email, $sujet, $message)) {
+        // Vérification des champs non vides
+        if (!empty($nom) && !empty($email) && !empty($sujet) && !empty($message)) {
+
+            // Affichage du résultat - Première occurrence
+            echo "<h3 class='text-light'> Soumission du formulaire réussie :</h3>";
+            echo "<p class='text-light'>Je m'appelle **$nom**, mon email est **$email**.</p> 
+                  <p class='text-light'>Ma plainte porte sur **$sujet**.</p> 
+                  <p class='text-light'>Contenu : <br> $message</p>";
+        } else {
+            // Optionnel : Afficher un message d'erreur si des champs sont vides
+            echo "<h3 class='text-danger Create selector'>Erreur : Veuillez remplir tous les champs du formulaire.</h3>";
+        }
+    }
+    ?>
 </main>
 <footer>
     <div class="container-fluid">
