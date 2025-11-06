b191de55762d1135824beb1462c350f0b881867e
rahinsoo <rahinsoo@etik.com>
2025-11-06 11:00:23 +0100

    TP02-PDO | protection si donnée vides ou présentes

--- TP02-PDO/traitement.php
+++ TP02-PDO/traitement.php
@@ -4,4 +4,21 @@ $email = $_POST['mail'];
 $sujet = $_POST['sujet'];
 $message = $_POST['message'];
 
-echo "je m'appelle $nom nom email est $email . Ma plainte porte sur $sujet <br> contenu : <br> $message";
+if ( isset($nom) && isset($email) && isset($sujet) && isset($message)){
+    if (empty($nom)){
+        echo "le nom est vide <br>";
+    }
+    if (empty($email)){
+        echo "l'email est vide <br>";
+    }
+    if (empty($sujet)){
+        echo "le sujet est vide <br>";
+    }
+    if (empty($message)){
+        echo "le message est vide <br>";
+    }
+    if (!empty($nom) && !empty($email) && !empty($sujet) && !empty($message)){
+        echo "je m'appelle $nom nom email est $email . Ma plainte porte sur $sujet <br> contenu : <br> $message";
+    }
+
+}
