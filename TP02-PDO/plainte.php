ece51af843edb95b3f6acc9cc36d4d4244fecf55
rahinsoo <rahinsoo@etik.com>
2025-11-07 16:50:29 +0100

    Modification plainte.php - Ajout supression selective V.08

--- TP02-PDO/plainte.php
+++ TP02-PDO/plainte.php
@@ -3,20 +3,20 @@ global $bdd;
 require_once "bdd.php";
 
 // Supprimer une entité
-if(isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['action']) && $_GET['action']=='supprimer'){
+if (isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['action']) && $_GET['action'] == 'supprimer') {
     $id = $_GET['id'];
     $sql = "DELETE FROM plainte WHERE id = :id";
     $query = $bdd->prepare($sql);
     $verif = $query->execute(['id' => $id]);
 
-    if ($verif){
+    if ($verif) {
         header("Location: plainte.php");
         exit();
     }
 }
 
 // Gérer la visibilité (SÉPARÉ, pas imbriqué !)
-if(isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['visible']) && isset($_GET['action']) && $_GET['action']=='gerervisibilite') {
+if (isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['visible']) && isset($_GET['action']) && $_GET['action'] == 'gerervisibilite') {
     $visible = $_GET['visible'];
     $id = $_GET['id'];
 
@@ -35,39 +35,57 @@ if(isset($_GET['id']) && !empty($_GET['id']) && isset($_GET['visible']) && isset
         exit();
     }
 }
+if(isset($_GET['id_selected'])){
+    $id_selected = $_GET['id_selected'];
+    // vérifiez si la table n'est pas vide
+    if(count($id_selected) > 0 ){
+$sql = "DELETE FROM plainte WHERE id = :id";
+$query = $bdd->prepare($sql);
 
+// Suppression des id
+        foreach ($id_selected as $id_s){
+            $query->execute([
+                    'id' => $id_s // valeur actuelle
+            ]);
+        }
+    }
+}
 // Récupérer toutes les plaintes
 $sql = "SELECT * from plainte";
 $query = $bdd->query($sql);
 $plainte = $query->fetchAll();
 ?>
 
-    <!DOCTYPE html>
-    <html lang="fr">
+<!DOCTYPE html>
+<html lang="fr">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <!-- <link rel="stylesheet" href="assets/css/main.css"> -->
-        <title>Test formulaire PDO</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="assets/css/main.css"> -->
+    <title>Test formulaire PDO</title>
+</head>
 
-    <body class="bg-dark">
-    <header>
-        <div class="container text-center">
-            <h1 class="text-light">FOOOOrmuullaiiiire Plaaaaaaiiintes PDO</h1>
+<body class="bg-dark">
+<header>
+    <div class="container text-center">
+        <h1 class="text-light">FOOOOrmuullaiiiire Plaaaaaaiiintes PDO</h1>
+    </div>
+</header>
+<main>
+    <a href="formulaire.php">
+        <button type="button" class="btn btn-outline-secondary ">FOOORRRRMMMUUUULLLLAAAIIREEEEE</button>
+    </a>
+    <form action="plainte.php" method="GET">
+        <div class="form-group">
+            <button type="submit" class="btn btn-outline-danger">Check SUUUUPPPRRRREEESSSSIIIIOOOOONN</button>
         </div>
-    </header>
-    <main>
-        <a href="formulaire.php">
-            <button type="button" class="btn btn-secondary ">FOOORRRRMMMUUUULLLLAAAIIREEEEE</button>
-        </a>
-
         <table class="table">
             <thead>
             <tr>
+                <th>dot</th>
                 <th scope="col">id</th>
                 <th scope="col">nom</th>
                 <th scope="col">sujet</th>
@@ -79,15 +97,16 @@ $plainte = $query->fetchAll();
             </thead>
             <tbody>
             <?php
-            foreach ($plainte as $item) {?>
+            foreach ($plainte as $item) { ?>
                 <tr>
-                    <th scope="col"> <?php echo $item['id']?> </th>
-                    <th scope="col"> <?php echo $item['nom']?> </th>
-                    <th scope="col"> <?php echo $item['sujet']?> </th>
-                    <th scope="col"> <?php echo $item['message']?> </th>
-                    <th scope="col"> <?php echo $item['date_plainte']?> </th>
+                    <th><input type="checkbox" name="id_selected[]" id="" value="<?php echo $item['id']; ?>"></th>
+                    <th scope="col"> <?php echo $item['id']; ?> </th>
+                    <th scope="col"> <?php echo $item['nom']; ?> </th>
+                    <th scope="col"> <?php echo $item['sujet']; ?> </th>
+                    <th scope="col"> <?php echo $item['message']; ?> </th>
+                    <th scope="col"> <?php echo $item['date_plainte']; ?> </th>
                     <th scope="col">
-                            <?php echo ($item['visible'] == 1) ? "<span class='btn btn-success'> visible</span>" : "<span class='btn btn-danger'> invisible</span>"; ?>
+                        <?php echo ($item['visible'] == 1) ? "<span class='btn btn-success'> visible</span>" : "<span class='btn btn-danger'> invisible</span>"; ?>
                     </th>
                     <th scope="col">
                         <a href="plainte.php?id=<?php echo $item['id']; ?>&action=supprimer">
@@ -103,18 +122,17 @@ $plainte = $query->fetchAll();
                 </tr>
             <?php }
             ?>
-
-
             </tbody>
         </table>
-    </main>
-    <footer>
-        <div class="container-fluid">
-            <div class="row text-center text-bg-dark">
-                <div class="col">Créé par Rah Insoo en 2025</div>
-            </div>
-        </div>
-    </footer>
-    </body>
+    </form>
 
-    </html>
+</main>
+<footer>
+    <div class="container-fluid">
+        <div class="row text-center text-bg-dark">
+            <div class="col">Créé par Rah Insoo en 2025</div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
